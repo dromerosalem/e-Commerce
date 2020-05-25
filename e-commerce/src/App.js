9b753c6a6067a0cc1614a0431001eff7cb4edbd8
dromerosalem <58950426+dromerosalem@users.noreply.github.com>
2020-05-25 16:09:15 +0100

    Directory and MenuItem components

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './HomePage'
+import HomePage from './page/homepage/HomePage'
 
 function App() {
   return (
---
+++ e-commerce/src/page/homepage/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './homePage.scss'
+
+import Directory from '../../components/directory/Directory'
+
+
+const HomePage = () => (
+  <div className='homepage'>
+    <h1>Welcome to my Homepage</h1>
+    <Directory/>
+  </div>
+)
+
+
+
+export default HomePage
